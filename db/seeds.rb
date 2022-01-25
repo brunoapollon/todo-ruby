4a773158a9bd9b3429acc1da5b80ea6e1ff340ae
Bruno Lopes <eusoubruno7@gmail.com>
2022-01-25 11:02:09 -0300

    feat: adding cors and tasks in db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Task.create(title: 'Ir ao mercado', done: false)
+Task.create(title: 'Ler one piece na sexta', done: false)
+Task.create(title: 'Estudar ruby', done: false)
+Task.create(title: 'Ler sobre código limpo', done: false)
+Task.create(title: 'ler sobre arquitetura limpa', done: false)
+Task.create(title: 'Consumir api ruby com react', done: false)
+Task.create(title: 'Estudar sobre ruby on rails com postgress', done: true)
+Task.create(title: 'Estudar React', done: true)
